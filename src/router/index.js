2893de6d0846dc9a29cb3f7057ccb7a43f98a597
Anoob Bava <anoob.bava@gmail.com>
2019-03-16 11:53:32 +0530

    Added login part

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Register from '@/components/auth/Register.vue'
+import Signin from '@/components/auth/Signin.vue'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/register',
       name: 'Register',
       component: Register
+    },
+    {
+      path: '/login',
+      name: 'Signin',
+      component: Signin
     }
   ]
 })
